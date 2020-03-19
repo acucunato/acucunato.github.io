9cb53f09d2f384ca264694ab97d547c88c16739d
Alyssa Cucunato <acucunato18@gmail.com>
2020-03-19 15:02:01 -0400

    added scrollspy functionality

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
 $(document).ready(function() {
-  $("body").scrollspy({ target: ".navbar", offset: 50 });
+  $("body").scrollspy({ target: "#main-navbar", offset: 105 });
 
-  // Add smooth scrolling on all links inside the navbar
-  $(".scroll-text a").on("click", function(event) {
-    debugger;
-    // Make sure this.hash has a value before overriding default behavior
+  // Smooth Scrolling
+  $("#main-navbar a").on("click", function(event) {
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
       event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+      const hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $("html, body").animate(
         {
           scrollTop: $(hash).offset().top
         },
         800,
         function() {
-          // Add hash (#) to URL when done scrolling (default click behavior)
           window.location.hash = hash;
         }
       );
-    } // End if
+    }
   });
 });
