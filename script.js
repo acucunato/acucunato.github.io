971020f6ecd26819c4e4e44bdd17c2bd11eeb4a3
Alyssa Cucunato <acucunato18@gmail.com>
2020-03-20 11:57:49 -0400

    updated scrollspy

--- script.js
+++ script.js
@@ -80,6 +80,26 @@ $(document).ready(function() {
     }
   });
 
+  $("body").scrollspy({ target: ".my-work", offset: 105 });
+
+  $(".my-work a").on("click", function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      const hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+
   var navbar = document.querySelector("nav");
 
   window.onscroll = function() {
