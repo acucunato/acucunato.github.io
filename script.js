ef22011741f327e93d8cca51ab889ec348aaf77a
Alyssa Cucunato <acucunato18@gmail.com>
2020-03-20 11:35:24 -0400

    color change on scroll

--- script.js
+++ script.js
@@ -80,6 +80,16 @@ $(document).ready(function() {
     }
   });
 
+  var navbar = document.querySelector("nav");
+
+  window.onscroll = function() {
+    // pageYOffset or scrollY
+    if (window.pageYOffset > 0) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  };
   wow = new WOW({
     boxClass: "wow", // default
     animateClass: "animated", // default
