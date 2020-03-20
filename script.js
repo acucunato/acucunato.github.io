d9978bdb90a09dc483e5eebe876fb30f83e223ef
Alyssa Cucunato <acucunato18@gmail.com>
2020-03-19 21:51:06 -0400

    added wow

--- script.js
+++ script.js
@@ -19,4 +19,33 @@ $(document).ready(function() {
       );
     }
   });
+
+  $("body").scrollspy({ target: "#bio-btn", offset: 105 });
+
+  $("#bio-btn a").on("click", function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      const hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+  wow = new WOW({
+    boxClass: "wow", // default
+    animateClass: "animated", // default
+    offset: 0, // default
+    mobile: true, // default
+    live: true // default
+  });
+
+  new WOW().init();
 });
